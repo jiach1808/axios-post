d11d1b04fc5822ee4cdbe1e580135c82055633b5
wjc <2396554126@qq.com>
2019-10-07 21:57:27 +0800

    method

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios  from 'vue-axios'
+Vue.config.productionTip = false
+Vue.use(VueAxios,axios)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
